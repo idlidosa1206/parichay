9a9c162d7956bccf19e67d0038e77e32221c7d4b
Joyee2004 <128905872+Joyee2004@users.noreply.github.com>
2024-04-06 14:48:01 +0530

    Add files via upload

---
+++ frontend/src/onetoone.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import "./chat.css"
+const Chat = ({ messages, onSendMessage, currentUser, otherUser }) => {
+  const [inputText, setInputText] = useState('');
+
+  const handleSendMessage = () => {
+    if (inputText.trim() !== '') {
+      onSendMessage(inputText, currentUser); // Sending message as current user
+      setInputText('');
+    }
+  };
+
+  return (
+    <div>
+      <div className="chat-messages">
+        {messages.map((message, index) => (
+          <div key={index} className={`message ${message.sender === currentUser ? 'sent' : 'received'}`}>
+            <div className="username">{message.sender === currentUser ? currentUser : otherUser}</div>
+            <div className="message-text">{message.text}</div>
+          </div>
+        ))}
+      </div>
+      <div className="chat-input">
+        <input
+          type="text"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          placeholder="Type your message..."
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
