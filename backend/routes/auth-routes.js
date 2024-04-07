a236e11a619acd6401c07ea114d9f54097523070
Sayali <124203472+say2410@users.noreply.github.com>
2024-04-07 05:45:31 +0530

    Update auth-routes.js

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -3,13 +3,14 @@ const passport = require('passport');
 
 //auth login
 router.get('/login', (req,res)=>{
-    res.render('login');
+    res.render('login', {user: req.user});
 });
 
 //auth logout
 router.get('/logout', (req, res)=>{
     // handle with passport
-    res.send('logging out');
+    res.logout();
+    res.redirect('/');
 })
 
 //auth with google
@@ -19,7 +20,8 @@ router.get('/google', passport.authenticate('google',{
 
 //callback route for google to redirect to
 router.get('/google/redirect', passport.authenticate('google'),(req, res)=>{
-    res.send('you reached the call back URI');
+    //res.send(req.user);
+    res.redirect('/profile');
 })
 
 module.exports= router;
