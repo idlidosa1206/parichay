902f41984c36993579b8ec8fa338c03a9da63bae
Sayali <124203472+say2410@users.noreply.github.com>
2024-04-07 05:51:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,12 @@ import Navbar from "./Components/Navbar";
 import Home from "./pages/Home.js";
 import Footer from "./Components/Footer.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Help_page from "./pages/help_page.js";
-import Helpline from "./pages/helpline.js";
+import ChatPage from "./pages/chatPage.js";
 import Chat from "./pages/chat.js";
 import Profile from "./pages/profile.js";
 import Jobs from "./pages/jobs.js";
 import House from "./pages/houses.js";
-import EducationApp from "./pages/education.js"
+import EducationApp from "./pages/education.js";
 import Healthcare from "./pages/healthcare.js";
 
 const App = () => {
@@ -20,14 +19,13 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/home" element={<Home />} />
-          <Route path="/help_page" element={<Help_page />} />
-          <Route path="/helpline" element={<Helpline />} />
+          <Route path="/chatpage" element={<ChatPage />} />
           <Route path="/chat" element={<Chat />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/jobs" element={<Jobs />} />
           <Route path="/houses" element={<House />} />
           <Route path="/healthcare" element={<Healthcare />} />
-          <Route path="education" element={<EducationApp/>}/>
+          <Route path="education" element={<EducationApp />} />
           <Route index element={<Home />} />
         </Routes>
       </Router>
