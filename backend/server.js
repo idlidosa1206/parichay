02b1779e539d873145d412bd190f9121f2edad7b
Sayali <124203472+say2410@users.noreply.github.com>
2024-04-07 05:47:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,12 @@ const path = require('path');
 const ejs = require('ejs');
 const router =express.Router();
 const authRoutes = require('./routes/auth-routes.js');
-const passportSetup = require('./config/auth.js')
+const profileRoutes = require('./routes/profile-routes.js');
+const passportSetup = require('./config/auth.js');
+const cookieSession = require('cookie-session');
+const keys=require('./config/keys')
+const passport = require('passport');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,8 +18,17 @@ const PORT = process.env.PORT || 5000;
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
+app.use(cookieSession({
+    maxAge: 24*60*60*1000,
+    keys:[keys.session.cookieKey]
+}))
+
+app.use(passport.initialize())
+app.use(passport.session());
+
 // set up auth routes
 app.use('/auth',authRoutes);
+app.use('/profile',profileRoutes);
 
 // Middleware
 app.use(express.json());
@@ -23,7 +37,7 @@ app.use(express.json());
 
 
 app.get('/', (req,res)=>{
-    res.render('home');
+    res.render('home', {user:req.user});
 });
 
 
